1b604065726e3f10c7b68f944f5dc4e2d78c90c6
NightRavin22 <73198686+NightRavin22@users.noreply.github.com>
2021-01-14 15:54:35 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var cars, car1, car2, car3, car4;
+
+function preload(){
+track=loadImage("images/track.jpg");
+carImage1=loadImage("images/car5.png");
+carImage2=loadImage("images/car6.png");
+carImage3=loadImage("images/car7.png");
+carImage4=loadImage("images/car8.png");
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+  if(gameState === 2){
+    game.end();
+  }
+}
